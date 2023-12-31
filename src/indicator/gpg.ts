de6f99f6e094689cd5f8019aa50bdbc9d0b6a596
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2023-12-31 15:27:36 -0500

    Copy working parseKeyRecords function.
    Verified indepently at https://github.com/AlexanderAllen/zealous-feynman

--- src/indicator/gpg.ts
+++ src/indicator/gpg.ts
@@ -143,16 +143,23 @@ function parseIdentities(rawText: string): Array<IdentityRecord> {
     return identities;
 }
 
-function parseKeyRecords(rawText: string) {
-    // Match all non-revoked identities.
-    const recordPattern: RegExp = /(?<KeyRecordType>(?<fieldKeyType>pub|sub):(?<fieldKeyStatus>[^:]*):(?<fieldLength>[^:]*):(?<fieldPubKeyAlgo>[^:]*):(?<fieldKeyID>[^:]*):(?<fieldCreated>[^:]*):(?<fieldExpires>[^:]*):(?<fieldTrust>[^:]*):(?<fieldOwnerTrust>[^:]*):(?<fieldUserID>[^:]*):(?<fieldSigClass>[^:]*):(?<fieldCapability>[escaD?]+)\w*:(?:[^:]*:){4}(?<fieldCurveName>[^:]*):(?<fieldRest>[:\d]*)\n?)(?<FingerprintRecordType>^(?:fpr|fp2):(?:[^:]*:){8}(?<fingerprint>\w*):(?:[^:]*:)*?\n?)?(?<GripRecordType>^grp:(?:[^:]*:){8}(?<grip>\w*):(?:[^:]*:)*?\n?)?(?<IdentityRecordType>uid:(?=u)(?<fieldIdentityStatus>[^:]):(?:[^:]*):{3}(?<fieldIdentityCreated>[^:]*)(?:[^:]*):{2}(?<fieldIdentityID>[^:]*)(?:[^:]*):{2}(?<fieldIdentityComment>[^:]*):(?<fieldIdentityRest>[:\d]*)\n?)*/mg;
-    let matchedIdentities: RegExpExecArray | null;
-    let identities: Array<IdentityRecord> = [];
+/**
+ * Parse GPG record fields usign a regular expression.
+ *
+ * @param rawText Raw GPG output.
+ *
+ * @returns [] Array of parsed GPG records.
+ */
+export function parseKeyRecords(rawText: string) {
+
+    const recordPattern = /(?<KeyRecordType>(?<fieldKeyType>pub|sub):(?<fieldKeyStatus>[^:]*):(?<fieldLength>[^:]*):(?<fieldPubKeyAlgo>[^:]*):(?<fieldKeyID>[^:]*):(?<fieldCreated>[^:]*):(?<fieldExpires>[^:]*):(?<fieldTrust>[^:]*):(?<fieldOwnerTrust>[^:]*):(?<fieldUserID>[^:]*):(?<fieldSigClass>[^:]*):(?<fieldCapability>[escaD?]+)\w*:(?:[^:]*:){4}(?<fieldCurveName>[^:]*):(?<fieldRest>[:\d]*)\n?)(?<FingerprintRecordType>^(?:fpr|fp2):(?:[^:]*:){8}(?<fingerprint>\w*):(?:[^:]*:)*?\n?)?(?<GripRecordType>^grp:(?:[^:]*:){8}(?<grip>\w*):(?:[^:]*:)*?\n?)?(?<IdentityRecordType>uid:(?=u)(?<fieldIdentityStatus>[^:]):(?:[^:]*):{3}(?<fieldIdentityCreated>[^:]*)(?:[^:]*):{2}(?<fieldIdentityID>[^:]*)(?:[^:]*):{2}(?<fieldIdentityComment>[^:]*):(?<fieldIdentityRest>[:\d]*)\n?)*/mg;
+    let matchedIdentities;
+    const identities = [];
 
     while ((matchedIdentities = recordPattern.exec(rawText)) !== null) {
-        let test = null;
+        identities.push(matchedIdentities?.groups);
     }
-
+    return identities;
 }
 
 /**
