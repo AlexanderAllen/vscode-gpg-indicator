5331a5542692b54c7992f7e59e95b2a0406f9bd6
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2023-12-31 22:38:58 -0500

    Issue #7 - Initial type ambient file.

---
+++ src/indicator/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
+// Project: [~THE PROJECT NAME~]
+// Definitions by: [~YOUR NAME~] <[~A URL FOR YOU~]>
+
+interface GpgKeyInfo {
+  type: string;
+  capabilities: string;
+  fingerprint: string;
+  keygrip: string;
+  userId?: string;
+}
+
+interface IdentityRecord {
+  IdentityRecordType?: string;
+  fieldIdentityID?: number;
+  fieldIdentityStatus?: string;
+  fieldIdentityComment?: string;
+  fieldIdentityCreated?: string;
+  fieldIdentityRest?: string;
+}
